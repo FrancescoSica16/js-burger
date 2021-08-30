c1a48862f09090d333171f3772098c59b7c81a7a
FrancescoSica16 <francesco.sica16@gmail.com>
2021-08-30 17:13:30 +0200

    html e start js

---
+++ myscript.js
@@ -0,0 +1,16 @@
+
+let nome= 0;
+
+let prezzoBase = 5;
+
+let prezzoFinale = 0;
+
+const formaggio = 3;  const uova = 1; const pomodoro = 1; 
+const lattuga = 1; const mostarda = 2; const ketchup = 2;
+
+
+nome = document.getElementById("nome-panino").
+    nome = 
+});
+
+console.log(nome)
