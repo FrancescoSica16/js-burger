3f3143437bb776db5951b6b2d20c7cbab4d7d66c
FrancescoSica16 <francesco.sica16@gmail.com>
2021-08-31 09:35:59 +0200

    fine

--- myscript.js
+++ myscript.js
@@ -1,16 +1,57 @@
 
-let nome= 0;
-
-let prezzoBase = 5;
+const prezzoBase = 5;
 
 let prezzoFinale = 0;
 
-const formaggio = 3;  const uova = 1; const pomodoro = 1; 
-const lattuga = 1; const mostarda = 2; const ketchup = 2;
+const couponList = ["utente20", "influencer20", "bonus20"];
+
+let bottone = document.getElementById("calcolo");
+console.log(bottone);
+
+let nomePanino = document.getElementById("nome-panino");
+
+console.log(nomePanino);
+
+bottone.addEventListener('click', 
+function () {
+    if (nomePanino.value.length === 0 ) {
+        alert("inserisci nome");
+
+    }
+    else //se c'è un nome 
+    {
+        let ingredientiAggiuntivi = document.getElementsByClassName("list-item");
+        console.log(ingredientiAggiuntivi);
+
+        prezzoFinale == 5;
+
+        for (let i = 0; i < ingredientiAggiuntivi.length; i++) {
+
+            const elementoAggiunto = ingredientiAggiuntivi[i];
+            console.log(elementoAggiunto);
+
+            if (elementoAggiunto.checked) {
+                
+                prezzoFinale += parseInt(elementoAggiunto.value);
+            }
 
 
-nome = document.getElementById("nome-panino").
-    nome = 
-});
+            let coupon = document.getElementById("coupon") ;
+            console.log(prezzoFinale)
+            if (coupon.value.length > 0 ) {
+                
+               if (couponList.includes(coupon.value)) {
+                   prezzoFinale = prezzoFinale * 0.8;
+               } else {
+                  console.log("il coupon non essite") 
+               }
 
-console.log(nome)
+            }
+
+            let totale = document.getElementById("somma").innerHTML = prezzoFinale ;
+            
+        }
+      
+console.log(prezzoFinale)
+    }
+})
